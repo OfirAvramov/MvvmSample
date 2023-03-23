15e34ee4f40bf9b67afed7ccffb5cd10bd117c10
User <User@LAB3-1>
2023-03-23 15:04:06 +0200

    Ghij

--- MvvmSample/ViewModels/MainPageViewModel.cs
+++ MvvmSample/ViewModels/MainPageViewModel.cs
@@ -12,16 +12,9 @@ using System.Windows.Input;
 
 namespace MvvmSample.ViewModels
 {
-    public class MainPageViewModel : INotifyPropertyChanged
+    public class MainPageViewModel : ViewModelBase
     {
-        #region מימוש ממשק עדכון שינויים
-        public event PropertyChangedEventHandler PropertyChanged;                        
-        
-        private  void OnPropertyChanged([CallerMemberName] string propertyName="")
-        {
-             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));         
-        }
-        #endregion
+      
 
 
         public ObservableCollection<Student> Students { get; set; }
@@ -30,18 +23,36 @@ namespace MvvmSample.ViewModels
 
         public ICommand ClearDataCommand { get; private set; }
 
+        public ICommand RefreshCommand { get; private set; }
+
+        string name;
+        public string CurrentName { get { return name; } set { if(name!= value) { name = value; OnPropertyChanged(); } } }
+
+        DateTime date;
+        public DateTime CurrentDate { get { return date; } set { if (date != value) { date = value; OnPropertyChanged(); } } }
+
+        bool isRefreshing;
+        public bool IsRefreshing { get { return isRefreshing; } set { if (isRefreshing != value) { isRefreshing = value; OnPropertyChanged(); } } }
+
+        public ICommand AddStuCommand { get; private set; }
         public MainPageViewModel()
         {
             Students = new ObservableCollection<Student>(); 
             LoadDataCommand=new Command(LoadData);
             ClearDataCommand = new Command(() => Students.Clear());
+            RefreshCommand = new Command(LoadData);
+            AddStuCommand = new Command(()=>Students.Add(new Student() { BirthDate=CurrentDate, Image=$"{CurrentName}.jpg", Name=CurrentName}));
+            
         }
 
         public void LoadData()
         {
             Students.Clear();
            Students.Add(new Student() { BirthDate = DateTime.Now, Name = "ג'ופיר", Image = "jofir.jpg" });
+            Students.Add(new Student() { BirthDate = DateTime.Now, Name = "ג'ופיר", Image = "jofir.jpg" });
+            IsRefreshing = false;
             
         }
+
     }
 }
